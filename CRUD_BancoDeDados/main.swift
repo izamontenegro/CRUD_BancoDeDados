72fee2067c400b1d551e37b42de02130267b11be
Bianca Oliveira <biadnasc@gmail.com>
2025-05-07 08:26:02 -0300

    Read function

--- CRUD_BancoDeDados/main.swift
+++ CRUD_BancoDeDados/main.swift
@@ -7,7 +7,7 @@
 // MARK: - Models
 import Foundation
 
-//FuncionarioManager.create(primeiroNome: "Izadora", inicialDoMeio: nil, ultimoNome: "Montenegro", cpf: "02902713207", salario: 1500, genero: .female, endereco: "Senador pompeu", dataDeNascimento: "06/12/2005", departamento: nil, projetos: nil)
+//FuncionarioManager.create(primeiroNome: "Maria", inicialDoMeio: nil, ultimoNome: "Silva", cpf: "12345678901", salario: 1500, genero: .female, endereco: "Rua dos Bobos", dataDeNascimento: "01/01/2005", departamento: nil, projetos: nil)
 //
 //// da pra tentar filtrar o funcionario por nome, cpf sla
 //ProjetoManager.create(nome: "Projeto 1", local: "Casa", funcionarios: [FuncionarioManager.funcionarios[0]])
